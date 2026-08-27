0606d24269384eabe38965e8411d3e10170c3f48
QiaoLei <791698324@qq.com>
2026-08-27 14:46:11 +0800

    修改预览页面

--- frontend/src/components/Preview.vue
+++ frontend/src/components/Preview.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-let oldNames = ref<string[]>([]);
-let newNames = ref<string[]>([]);
+import {dataCenter} from "../dataCenter";
 </script>
 
 <template>
   <div id="preview" class="preview" >
     <div class="preview-title">
-      <p>预览</p>
+      <h2>预览</h2>
+      <hr style="width: 100%; color: #3e3e3e;">
       <div class="preview-description">
         <div>原始名</div>
         <div>→</div>
@@ -15,7 +14,15 @@ let newNames = ref<string[]>([]);
       </div>
     </div>
 
-    <section>
+    <section class="preview-content">
+      <div v-for="(preview, index) in dataCenter.preview" :key="index" class="preview-item">
+        <div class="item">
+          <p>{{ preview.OldDisPlayName }}</p>
+          <p>-></p>
+          <p>{{ preview.NewDisPlayName }}</p>
+        </div>
+
+      </div>
     </section>
   </div>
 </template>
@@ -36,16 +43,46 @@ let newNames = ref<string[]>([]);
 
 .preview-title{
   display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  font-size: 1.2rem;
+  flex-direction: column;
+  align-items: flex-start;
   font-weight: bold;
-  padding: 10px;
+  font-size: 1rem;
+  background-color: rgb(35, 41, 57);
+  border-bottom: 1px solid #676767;
+  text-align: left;
+}
+
+h2{
+  font-size: 1rem;
+  padding-left: 1rem;
+  margin-bottom: 0.5rem;
 }
 
 .preview-description{
+  width: 90%;
   display: flex;
   flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin:0 auto 0.5rem;
+}
+
+.preview-content{
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-top:-0.5rem;
+  width: 100%;
+}
+.preview-item{
+  height: 1.5rem;
+  width: 100%;
+}
+.item{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  overflow: hidden;
 }
 </style>
